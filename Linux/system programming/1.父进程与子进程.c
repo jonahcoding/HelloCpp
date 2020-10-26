e9f80d22f786e716947ab9368943e071a4e28662
jonahcoding <714092359@qq.com>
2020-10-26 22:08:38 +0800

    C++

---
+++ Linux/system programming/1.父进程与子进程.c
@@ -0,0 +1,41 @@
+/*
+--------------------------------------------------
+【程序与进程】
+    程序：一组指令的有序集合，静态实体，不占用系统资源。
+    进程：具有一定独立功能的程序关于某个数据集合上的一次运行活动，动态实体，占用系统资源（有生命周期）。
+    并发：进程并行运行，轮流使用系统资源。
+--------------------------------------------------
+pid_t：进程号类型
+getpid()：返回当前进程的ID
+getppid()：返回当前进程的父进程的ID
+--------------------------------------------------
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main()
+{
+    pid_t pid;
+    printf("xxxxxxxxxx\n");
+
+    pid = fork();
+    if (pid == -1)
+    {
+        perror("fork error:");
+        exit(1);
+    }
+    else if (pid == 0)
+    {
+        printf("I'm Child, pid = %u, ppid = %lu\n", getpid(), getppid());
+    }
+    else
+    {
+        printf("I'm Parent, pid = %u, ppid = %lu\n", getpid(), getppid());
+    }
+    
+    printf("YYYYYYYYYYYYYYYYYY\n");
+
+    return 0;
+}
