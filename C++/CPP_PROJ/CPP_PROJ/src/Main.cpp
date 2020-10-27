008a774242f5a0daf446097b27253cc092a51d15
jonahcoding <714092359@qq.com>
2020-10-28 00:30:11 +0800

    内存管理

--- C++/CPP_PROJ/CPP_PROJ/src/Main.cpp
+++ C++/CPP_PROJ/CPP_PROJ/src/Main.cpp
@@ -1,11 +1,49 @@
+#define _CRT_SECURE_NO_WARNINGS
+
 #include <iostream>
 
-#include "1.2 namespace.h"
+using namespace std;
+
+class Person1
+{
+public:
+	int mAge;
+	char* pName;
+
+	Person1()
+	{
+		mAge = 20;
+
+		pName = (char*)malloc(strlen("Jonah") + 1);
+		if (pName != NULL)
+		{
+			strcpy(pName, "Jonah");
+			cout << "malloc" << endl;
+		}
+		else
+		{
+			cout << "ÉêÇëÄÚ´æÊ§°Ü£¡";
+		}
+	}
+};
+
+void test_malloc()
+{
+	//·ÖÅäÄÚ´æ
+	Person1* person = (Person1*)malloc(sizeof(Person1));
+	if (person == NULL) {}
+	else {}
+}
+
+
+
+
+
 
 
 int main()
 {
-
+	test_malloc();
 
 	system("pause");
 	return 0;
